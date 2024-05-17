d996d44f68b0f14aae04cfd9c1507c46f1f67e34
Eduard Schumacher <107512513+mijamoto911@users.noreply.github.com>
2024-05-17 11:07:15 +0200

    correct function

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
-  return;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 console.log(getShippingMessage('Australia ', 120, 50));
 console.log(getShippingMessage('Germany ', 80, 20));
